4fc74b50fd6ef830219a854289ed50a2eeae8425
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-13 23:53:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,3 +8,16 @@ def using_insert (array , element)
   array.insert(element)
   
 end 
+
+def using_uniq (array)
+  array.uniq()
+  
+end 
+
+
+
+def using_flatten (array )
+  array.flatten
+  
+end 
+
